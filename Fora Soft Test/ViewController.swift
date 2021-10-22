007e8ac4f1cfcacbddd9a45a1c1cabbbd7994e54
BDA7 <bondare-danila2017@yandex.ru>
2021-10-23 02:00:38 +0300

    Initial Commit

---
+++ Fora Soft Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fora Soft Test
+//
+//  Created by Данила Бондаренко on 23.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
